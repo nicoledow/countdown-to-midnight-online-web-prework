3c144ccf257898efc4bae24107d7a7af971dc93e
Nicole Dow <nikki.dow71@gmail.com>
2019-04-14 18:31:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,11 @@
 #write your code here
 
 def countdown(number_of_seconds)
-   number = number_of_seconds
+  number = number_of_seconds
   
-   while number >= 1
+  while number >= 1
     puts "#{number} SECOND(S)!"
     number -= 1 
-  end
   end
   return "HAPPY NEW YEAR!"
 end

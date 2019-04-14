56bf73f28bf67a1a1ec9012c7e5414866bf0d517
Nicole Dow <nikki.dow71@gmail.com>
2019-04-14 18:30:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,16 +3,11 @@
 def countdown(number_of_seconds)
    number = number_of_seconds
   
-
-  if number_of_seconds == 12
-    puts "HAPPY NEW YEAR"
-  else
    while number >= 1
     puts "#{number} SECOND(S)!"
     number -= 1 
-    sleep 1
   end
   end
-  sleep 1
   return "HAPPY NEW YEAR!"
 end
+
